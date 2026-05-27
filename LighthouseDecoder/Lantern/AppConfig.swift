cbc787d22849cccb2701d02eeda5b2963c5195f5
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-27 08:59:13 +0300

    Update AppConfig.swift

--- LighthouseDecoder/Lantern/AppConfig.swift
+++ LighthouseDecoder/Lantern/AppConfig.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 enum AppConfig {
-    static let beamAnchor = URL(string: "https://keitaro-zaglushka.com")!
-    static let privacyPolicyURL = URL(string: "https://hallowtommy.github.io/lighthouse-decoder-privacy")!
-    static let supportEmail = "support@hallowtommy.app"
+    static let beamAnchor = URL(string: "https://kaleydos.top/vPLWKF")!
+    static let privacyPolicyURL = URL(string: "https://www.termsfeed.com/live/ef1ea708-4808-466c-882d-1adb764f7b49")!
+    static let supportEmail = "ladykov1101@icloud.com"
 
     static var versionLine: String {
         let mv = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
